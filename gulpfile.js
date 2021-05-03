9812f5d329191631bfea852715fd8548c2973eae
Guillonaxel0714 <55197055+Guillonaxel0714@users.noreply.github.com>
2021-05-03 12:23:30 +0200

    set up front-end
    Mise en place de l'environnement front-end html/sass avec npm et gulp

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const { src, dest, watch, parallel } = require('gulp');
+const gulpSass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+function sync() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    })
+    watch('*.html').on('change', browserSync.reload);
+}
+
+function sass() {
+    return src('sass/styles.scss')
+    .pipe(gulpSass())
+    .pipe(dest('css/'))
+    .pipe(browserSync.stream())
+}
+
+function watcher(done) {
+    watch('sass/styles.scss', sass)
+    browserSync.reload();
+    done();
+}
+
+module.exports = {
+    sass,
+    watcher,
+    sync: parallel(sync, watcher)
+}
