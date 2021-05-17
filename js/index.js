7c866da145dd1800e5bebe3ee801659f2081ec69
Guillonaxel0714 <55197055+Guillonaxel0714@users.noreply.github.com>
2021-05-17 18:03:26 +0200

    Ajout de la page de contact
    Création front du formulaire de contact et la partie coordonnées GEDEM.

--- js/index.js
+++ js/index.js
@@ -213,3 +213,16 @@ const timeline = document.querySelector(".timeline ol"),
   }
 
 
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 48.542105, lng: 2.6554 },
+    zoom: 10,
+
+  });
+}
+
+
+
+
